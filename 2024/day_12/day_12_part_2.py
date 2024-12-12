cec0eb541b85ffbf851f14fa531f159d683ff91c
Amanda Azevedo <amanda.azevedo@shopify.com>
2024-12-12 15:41:50 +0100

    Improve code

--- 2024/day_12/day_12_part_2.py
+++ 2024/day_12/day_12_part_2.py
@@ -12,16 +12,11 @@ def bfs(map, x, y):
     current_plant = map[x][y]
     area = 1
 
-    top_sides = []
-    bottom_sides = []
-    left_sides = []
-    right_sides = []
-
     sides = {
-        (0, 1): right_sides,
-        (0, -1): left_sides,
-        (1, 0): bottom_sides,
-        (-1, 0): top_sides
+        (0, 1): [],
+        (0, -1): [],
+        (1, 0): [],
+        (-1, 0): []
     }
 
     while queue:
@@ -56,9 +51,9 @@ def count_sides(sides):
         x = 0
         y = 1
 
-        dir_x = index
+        anti_index = int(not index)
 
-        side_list = sorted(side_list, key=lambda x: x[int(not index)])
+        side_list = sorted(side_list, key=lambda x: x[anti_index])
 
         while x < len(side_list):
             while y < len(side_list):
@@ -66,30 +61,18 @@ def count_sides(sides):
                 length = side_list[x][2]
                 compare_side = (side_list[y][0], side_list[y][1])
 
-                # horizontal movement
-                if dir_x == 0:
-                    # if it's not equal means the sides are not in the same row
-                    if compare_side[0] != side[0]:
-                        y += 1
-                        continue
+                if compare_side[index] != side[index]:
+                    y += 1
+                    continue
 
-                    # if the sides are in the same row, check if they are adjacent
-                    if abs(compare_side[1] - side[1]) == length:
-                        side_list.pop(y)
+                if abs(compare_side[anti_index] - side[anti_index]) == length:
+                    side_list.pop(y)
+
+                    if index == 0:
                         side_list[x] = (side[0], min(side[1], compare_side[1]), length + 1)
-                        continue
-                # vertical movement
-                else:
-                    # if it's not equal means the sides are not in the same column
-                    if compare_side[1] != side[1]:
-                        y += 1
-                        continue
-
-                    # if the sides are in the same colum, check if they are adjacent
-                    if abs(compare_side[0] - side[0]) == length:
-                        side_list.pop(y)
+                    else:
                         side_list[x] = (min(side[0], compare_side[0]), side[1], length + 1)
-                        continue
+                    continue
                 y += 1
             x += 1
             y = x + 1
@@ -102,5 +85,3 @@ map = parse_input('input.txt')
 cost = get_cost(map, bfs)
 
 print(cost)
-
-# bfs(map, 4, 9)
