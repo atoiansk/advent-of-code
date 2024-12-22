b05f18c760f83f1cf676fff23215e5a35729641e
Amanda Azevedo <amanda.azevedo@shopify.com>
2024-12-22 11:08:07 +0100

    Day 22 Part 2

--- 2024/day_22/day_22.py
+++ 2024/day_22/day_22.py
@@ -1,13 +1,49 @@
 from monkey_market import MonkeyMarket
+from contextlib import contextmanager
+import time
+
+@contextmanager
+def timer():
+    start = time.perf_counter()
+    yield
+    end = time.perf_counter()
+    print(f"Execution time: {end - start} seconds")
 
 file = open("input.txt", "r")
 lines = file.readlines()
 
 # Part 1
-sum = 0
+with timer():
+    sum = 0
 
-for line in lines:
-    initial_number = int(line.strip('\n'))
-    sum += MonkeyMarket(initial_number, 2000).get_secret_number()
+    for line in lines:
+        initial_number = int(line.strip('\n'))
+        sum += MonkeyMarket(initial_number, 2000).get_last_secret_number()
 
-print(sum)
+    print(sum)
+
+# Part 2
+with timer():
+    price_changes_sequences = []
+    unique_sequences = set()
+
+    for line in lines:
+        initial_number = int(line.strip('\n'))
+        price_changes_sequence = MonkeyMarket(initial_number, 2000).get_change_sequences()
+        unique_sequences = unique_sequences | set(price_changes_sequence.keys())
+
+        price_changes_sequences.append(price_changes_sequence)
+
+    max_price = -1
+    max_price_sequence = []
+
+    for unique_sequence in unique_sequences:
+        sum = 0
+        for price_changes_sequence in price_changes_sequences:
+            if unique_sequence in price_changes_sequence:
+                sum += price_changes_sequence[unique_sequence]
+        if sum > max_price:
+            max_price = sum
+            max_price_sequence = unique_sequence
+
+    print(max_price, max_price_sequence)
--- 2024/day_22/monkey_market.py
+++ 2024/day_22/monkey_market.py
@@ -5,15 +5,42 @@ class MonkeyMarket:
         self.number = number
         self.n_numbers = n_numbers
 
-    def get_secret_number(self):
+    def get_last_secret_number(self):
         secret_number = self.number
 
         for i in range(self.n_numbers):
-            secret_number = self.prune(self.mix(secret_number * 64, secret_number))
-            secret_number = self.prune(self.mix(secret_number // 32, secret_number))
-            secret_number = self.prune(self.mix(secret_number * 2048, secret_number))
+            secret_number = self.get_secret_number(secret_number)
 
         return secret_number
+
+    def get_change_sequences(self):
+        secret_number = self.number
+        price_changes = []
+        price_changes_sequence = {}
+        last_price = 0
+
+        for i in range(self.n_numbers):
+            secret_number = self.get_secret_number(secret_number)
+            price = secret_number % 10
+
+            price_changes.append(price - last_price)
+
+            if i >= 4:
+                key = ((price_changes[i-3],price_changes[i-2]), (price_changes[i-1],price_changes[i]))
+
+                if key not in price_changes_sequence:
+                    price_changes_sequence[key] = price
+
+            last_price = price
+
+        return price_changes_sequence
+
+    def get_secret_number(self, number):
+        number = self.prune(self.mix(number * 64, number))
+        number = self.prune(self.mix(number // 32, number))
+        number = self.prune(self.mix(number * 2048, number))
+
+        return number
 
     def mix(self, value, secret_number):
         return value ^ secret_number
